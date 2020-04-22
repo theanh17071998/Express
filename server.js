fc111c20d1bcdc2dbe30fa2a369ea7e82ffc4e1a
Glitch (safe-interesting-workshop) <none>
2020-04-22 18:30:44 +0000

    Bai2 router params

--- server.js
+++ server.js
@@ -1,54 +1,56 @@
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser')
-const pug =  require('pug')
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+var express = require('express');
+var app = express();
+var low = require('lowdb')
+var shortid = require('shortid');
+var FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync('db.json')
-const db = low(adapter)
+var adapter = new FileSync('db.json')
+var db = low(adapter)
+var bodyParser = require('body-parser')
+var port = 3000;
 
-app.set('view engine', 'pug')
-app.set('views', './views')
-app.use(bodyParser.json()) 
-app.use(bodyParser.urlencoded({ extended: true })) 
 
-db.defaults({todos:[]}).write();
-app.get("/", (request, response) => {
-  response.send("I love CodersX");
-});
-app.get('/todos', (req, res) => {
-  res.render('index', {
-    todos: db.get('todos').value()
-  })
-});
-app.get('/todos/search', (req, res) => {
-  var q = req.query.q;
-  var matchedTodos= db.get('todos').value().filter(function(todo){
-    return todo.action.toLowerCase().indexOf(q.toLowerCase()) !== -1
-  });
-  res.render('index', {
-    todos: matchedTodos
-  });
-});
-app.get('/todos/create', function(req, res){
-    res.render('create')
-});
-app.get('/todos/:id', function(req, res){
-    var id = parseInt(req.params.id);
-    var todo = db.get('todos').find({id: id}).value();
-    db.get('todos').remove(todo).write()
-    res.render('index', {
-      todos:  db.get('todos').value()
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', function (req, res) { 
+    res.send("<h1>Hello coder.Tokyo</h1>");
+ })
+ db.defaults({"books": []}).write();
+ 
+ app.get('/books', function (req, res) {
+     res.render('index', {
+         books: db.get("books").value()
+     })
     });
-    res.redirect('/todos');
+
+app.get('/books/update/:id', function(req, res){ 
+    var id = req.params.id; 
+    var book = db.get('books').find({id: id}).value()
+    db.get('books').find(book).assign({ title: 'Năng và gió'}).write()
+    res.redirect('/books')
 });
 
+app.get('/books/delete/:id', function(req, res){ 
+var id = req.params.id; 
+var book = db.get('books').find({id: id}).value()
+db.get('books')
+  .remove(book)
+  .write()
+res.redirect('/books')
+});
 
-app.post('/todos/create', function(req, res){
-    db.get('todos').push(req.body).write();
-    res.redirect('/todos')
-})
-app.listen(process.env.PORT, () => {
-  console.log("Server listening on port " + process.env.PORT);
+app.get('/books/create', function (req, res) {
+    res.render('books/create')
+  }) 
+
+app.post('/books/create', function (req, res) {
+    req.body.id = shortid.generate(); 
+    db.get("books").push(req.body).write();    
+    res.redirect('/books'); 
+  })
+app.listen(port, function(){
+    console.log('server running with port' + port);
 });
