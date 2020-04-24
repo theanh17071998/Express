c66d8fffc7ae6b7f4a85d07f66d59d3954313566
Glitch (linen-small-chinchilla) <none>
2020-04-24 11:28:40 +0000

    Bai2_MVC

--- controllers/transactions.controller.js
+++ controllers/transactions.controller.js
@@ -2,39 +2,29 @@ const shortid = require('shortid');
 
 const db = require('../db')
 
-module.exports.index = (req, res) => {
-    res.render('users/index', {
+module.exports.index = function (req, res) {
+    res.render('transactions/index', {
+        transactions: db.get("transactions").value()
+    })
+   }
+module.exports.create =  function (req, res) {
+    res.render('transactions/create', {
+        books: db.get('books').value(),
         users: db.get('users').value()
-    })
-}
-module.exports.create = (req, res) => {
-    res.render('users/create')
-}
-module.exports.postCreate =  (req, res) => {
+    });
+   }
+module.exports.postCreate = function (req, res) {
+    var name = req.body.name;
+    var user = db.get('users').find({name: name}).value();
+    var title = req.body.title;
+    var book = db.get('books').find({title: title}).value();
     req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users')
-}
-module.exports.delete = (req, res) => {
-    var id = req.params.id;
-    var user = db.get('users').find({id: id}).value();
-    db.get('users').remove(user).write();
-    res.redirect('/users')
-}
-module.exports.postUpdate =  (req,res) => {
-    var id = req.params.id;
-    var user = db.get('users').find({id: id}).value();
-    db.get('users').find(user).assign({name: req.body.name, phone: req.body.phone }).write();
-    res.redirect('/users')
-}
-module.exports.update = (req, res) => {
-    var id = req.params.id;
-    var user = db.get('users').find({id: id}).value();
-    // db.get('users').find(user).assign({name: 'Nguyen Van A'}).write();
-    var userEdit = {};
-    userEdit.name = user.name
-    userEdit.phone = user.phone
-    res.render('users/update', {
-        user: userEdit
-    })
-}
+    req.body.userId = user.id;
+    req.body.bookId = book.id;
+    req.body.iscomplete = false;
+    db.get('transactions').push(req.body).write();
+    res.redirect('/transactions')
+  }
+ module.exports.complete = (req, res) => {
+    const id = req.params.id
+ }
--- routes/transactions.route.js
+++ routes/transactions.route.js
@@ -9,5 +9,6 @@ const route = express.Router();
 route.get('/', controller.index);
 route.get('/create', controller.create);
 route.post('/create', controller.postCreate);
+route.get('/:id/complete', controller.complete)
 
 module.exports = route;
