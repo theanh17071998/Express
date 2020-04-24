a4527377881e2e9b49ac9491fae79961b6f97baf
Glitch (concrete-free-settee) <none>
2020-04-24 10:59:10 +0000

    Bai1_MVC

--- routes/books.route.js
+++ routes/books.route.js
@@ -2,42 +2,14 @@ const express = require('express');
 const shortid = require('shortid');
 
 const db = require('../db')
+const controller = require('../controllers/books.controller')
 
 const route = express.Router();
 
-
-route.get('/', function (req, res) {
-    res.render('index', {
-        books: db.get("books").value()
-    })
-   });
-route.get('/update/:id', function(req, res){ 
-   var id = req.params.id; 
-   var book = db.get('books').find({id: id}).value()
-   db.get('books').find(book)
-   .assign({ title: 'Năng và gió'})
-   .write()
-   res.redirect('/books')
-});
-route.get('/delete/:id', function(req, res){
- 
-   var id = req.params.id; 
-   var book = db.get('books').find({id: id}).value()
-   db.get('books')
-   .remove(book)
-   .write()
-   res.redirect('/books')
-});
-route.get('/create', function (req, res) {
-   res.render('books/create')
- }) 
-
-route.post('/create', function (req, res) {
-   req.body.id = shortid.generate();
-   db.get("books").push(req.body).write();  
-   res.redirect('/books'); 
-  
-})
-
+route.get('/', controller.index);
+route.get('/update/:id', controller.update );
+route.get('/delete/:id', controller.delete);
+route.get('/create', controller.create) 
+route.post('/create', controller.postCreate)
 
 module.exports = route;
--- routes/transactions.route.js
+++ routes/transactions.route.js
@@ -2,30 +2,12 @@ const express = require('express');
 const shortid = require('shortid');
 
 const db = require('../db')
+const controller = require('../controllers/transactions.controller')
 
 const route = express.Router();
 
-route.get('/', function (req, res) {
-    res.render('transactions/index', {
-        transactions: db.get("transactions").value()
-    })
-   });
-route.get('/create', function (req, res) {
-    res.render('transactions/create', {
-        books: db.get('books').value(),
-        users: db.get('users').value()
-    });
-   });
-route.post('/create', function (req, res) {
-  var name = req.body.name;
-  var user = db.get('users').find({name: name}).value();
-  var title = req.body.title;
-  var book = db.get('books').find({title: title}).value();
-  req.body.id = shortid.generate();
-  req.body.userId = user.id;
-  req.body.bookId = book.id;
-  db.get('transactions').push(req.body).write();
-  res.redirect('/transactions')
-});
+route.get('/', controller.index);
+route.get('/create', controller.create);
+route.post('/create', controller.postCreate);
 
 module.exports = route;
