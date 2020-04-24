7b5a5eaf29780c93540c25261e3b4a545d1dc552
Glitch (ink-scalloped-dosa) <none>
2020-04-24 21:12:51 +0000

    B11_Bai1_StaticFile

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) 
+// app.use(express.static('https://ink-scalloped-dosa.glitch.me'))
 
 app.get('/', function (req, res) { 
     res.send("<h1>Hello coder.Tokyo</h1>");
