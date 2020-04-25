bee2b5d379df0f7dc6d8e79d5b5b1c8398dccc95
Glitch (zesty-polydactyl-deerstalker) <none>
2020-04-25 15:06:41 +0000

    B13_middleWare

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const shortid = require('shortid');
 
-const db = require('../db')
 const controller = require('../controllers/users.controller')
+const validate = require('../validate/user.validate')
 
 const route = express.Router();
 route.get('/', controller.index)
 route.get('/create', controller.create )
-route.post('/create', controller.postCreate)
+route.post('/create', validate.postCreate, controller.postCreate)
 route.get('/delete/:id', controller.delete )
 route.post('/update/:id', controller.postUpdate)
 route.get('/update/:id', controller.update)
